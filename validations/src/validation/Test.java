2984a016c164c0df2f9aecb7b7c987da287e8333
vasant <vasant@DESKTOP-MUP4AK5>
2019-04-01 23:07:59 +0530

    hi this is first application

---
+++ validations/src/validation/Test.java
@@ -0,0 +1,23 @@
+package validation;
+
+public class Test {
+
+	int  x=10;
+	void m1() {
+		Test obj=new Test();
+		System.out.println(obj.x);
+	}
+	public static void main(String[] args) {
+		
+		Test e1=new Test();
+		e1.x=15;
+		System.out.println(e1.x);
+		e1.x=20;
+		System.out.println(e1.x);
+		e1.m1();
+		
+		
+
+	}
+
+}
